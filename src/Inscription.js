79b8f29d069d9377f854014dff1df658bc6fc89f
SaraBenz <saraben943@gmail.com>
2021-03-16 14:10:56 +0100

    code inscription

--- src/Inscription.js
+++ src/Inscription.js
@@ -4,14 +4,30 @@ import { StylesProvider, createGenerateClassName } from '@material-ui/core/style
 import AppBar from '@material-ui/core/AppBar';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+import logo from "./images/logo.jpg";
+import educ from "./images/educ.svg";
 import {SiFacebook} from 'react-icons/si' ; 
-import {FcGoogle} from 'react-icons/fc'
+import {FcGoogle} from 'react-icons/fc';
+import {MdEmail}  from 'react-icons/md' ; 
+import {RiLockPasswordFill} from 'react-icons/ri';
+import {FaUserAlt} from 'react-icons/fa';
 
 
 export default function Inscription() {
-    const [Container,setContainer]=React.useState("container")
-    const[DivContainer,setDivContainer]=React.useState("forms-container")
+  //declaration des var 
+    const [Container,setContainer]=React.useState("container");
+    const [DivContainer,setDivContainer]=React.useState("forms-container");
+    const [nom , setNom] = React.useState("");
+    const [prenom , setPrenom] = React.useState("");
+    const [email , setEmail] = React.useState("");
+    const [emailc , setEmailc] = React.useState("");
+    const [pwdc , setPwdc] = React.useState("");
+    const [pwd , setPwd] = React.useState("");
+    const [mpwd , setMpwd] = React.useState("");
+
+    const [radio , setRadio] = React.useState("nr");
+
+
     const handleClickSignUp=()=>{ 
           setContainer(" container sign-up-mode")
           setDivContainer("forms-container sign-up-mode")
@@ -19,8 +35,45 @@ export default function Inscription() {
 
     const handleClickSignIn=()=>{
         setContainer(" container ")
-        setDivContainer("forms-container ")
+        setDivContainer("forms-container")
      }
+
+     
+
+     //modifier le contenu du champ ----nom----
+     const handleNom = e => {
+      //maitre a jour le state
+      setNom(e.target.value  ) }
+
+      //modifier le contenu du champ ----prenom----
+      const handlePrenom = e => {
+        //maitre a jour le state
+        setPrenom(e.target.value  ) }
+
+        //modifier le contenu du champ ----email----
+      const handleEmail = e => {
+        //maitre a jour le state
+        setEmail(e.target.value  ) }
+
+        //modifier le contenu du champ ----pwd----
+      const handlePwd = e => {
+        //maitre a jour le state
+        setPwd(e.target.value  ) }
+         //modifier le contenu du champ ----emailc----
+      const handleEmailc = e => {
+        //maitre a jour le state
+        setEmailc(e.target.value  ) }
+
+        //modifier le contenu du champ ----pwdc----
+      const handlePwdc = e => {
+        //maitre a jour le state
+        setPwdc(e.target.value  ) }
+
+        //modifier le contenu du champ ----mpwd----
+      const handleMpwd = e => {
+        //maitre a jour le state
+        setMpwd(e.target.value  ) }
+       
 
     return(
       <div>
@@ -28,42 +81,53 @@ export default function Inscription() {
       <div class={DivContainer}>
         <div class="signin-signup">
           <form action="#" class="sign-in-form">
+          <img src={logo}  style={{width:"15vh" ,float:"left",marginTop:'5px',marginRight:'20px', borderRadius: "50px"}} align="center" ></img>
             <h2 class="title">Se connecter</h2>
+
             <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Nom d'utilisateur" /> </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="mot de passe" /> </div>
+                <i><MdEmail/></i>
+                <input type="email" placeholder="Email" value={emailc} onChange={handleEmailc} required /> 
+            </div>
+              <div class="input-field">
+                <i ><RiLockPasswordFill/></i>
+                <input type="password" placeholder="mot de passe" value={pwdc} onChange={handlePwdc} required />
+              </div>
+
           <input type="submit" value="Connexion" class="btn solid" />
-            <p class="social-text">Connectez-vous avec</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-              <SiFacebook/>
-              </a>
-              <a href="#" class="social-icon">
-              <FcGoogle/>
-              </a></div>
           </form>
+
+          
           <form action="#" class="sign-up-form">
-            <h2 class="title">S'inscrire</h2>
+            <h3 class="title">S'inscrire</h3>
               <div class="input-field">
-                <i class="fas fa-user"></i>
-
-            <input type="text" placeholder="Nom d'utilisateur" /></div>
+                <i><FaUserAlt/></i>
+            <input type="text" placeholder="Nom" value={nom} onChange={handleNom}  required/>
+            </div>
+            <div class="input-field">
+                <i><FaUserAlt/></i>
+            <input type="text" placeholder="Prenom" value={prenom} onChange={handlePrenom} required/>
+            </div>
               <div class="input-field">
-                <i class="fas fa-envelope"></i>
-                <input type="email" placeholder="Email" /> </div>
+                <i><MdEmail/></i>
+                <input type="email" placeholder="Email" value={email} onChange={handleEmail} required/> </div>
               <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="mot de passe" /></div>
+                <i ><RiLockPasswordFill/></i>
+                <input type="password" placeholder="mot de passe" value={pwd} onChange={handlePwd} required/></div>
               <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Confirmer mot de passe" /></div>
-
+                <i ><RiLockPasswordFill/></i>
+                <input type="password" placeholder="Confirmer mot de passe" value={mpwd} onChange={handleMpwd} required/></div>
+                <div class="input">
+                <label class="social-text" >Vous êtes</label>
+               
+                 <label  class="social-text"> <input type="radio" onClick={() => setRadio(radio+1)} checked={radio===1 ? true : false}/>Enseignant </label> 
+                 <label class="social-text"><input type="radio" onClick={() => setRadio(radio+2)} checked={radio===2? true : false}/>Etudiant</label> 
+                
+                </div>
+                
             <input type="submit" class="btn" value="Créer" />
-              <p class="social-text">Inscrivez-vous avec</p>
+             
               <div class="social-media">
+              <p class="social-text">Inscrivez-vous avec</p>
                 <a href="#" class="social-icon">
                 <SiFacebook/>
                 </a>
@@ -78,14 +142,17 @@ export default function Inscription() {
         <div class="panel left-panel">
           <div class="content">
             <h3>Welcome To Uniswap</h3>
+           
             <p>
           Créez un compte et et swapez avec vos profs et vos amis !
             </p>
+            
             <button class="btn transparent" id="sign-up-btn" onClick={handleClickSignUp}>
                 Inscription
             </button>
+           
           </div>
-          <img src="img/log.svg" class="image" alt="" />
+          
         </div>
         <div class="panel right-panel">
           <div class="content">
